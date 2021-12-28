12ba1c8f1f938b304b954240b4e14591fae6aa99
Coding-A-Project <96562342+Coding-A-Project@users.noreply.github.com>
2021-12-28 14:01:50 +0530

    Add files via upload

---
+++ User login in python.py
@@ -0,0 +1,9 @@
+Username = input("Enter your username: ")
+if (Username == 'Coding_a_project'):
+        Password = input("Enter your password: ")
+        if (Password == 'Owner'):
+            print("Logged in Sucessfully")
+        else:
+            print("Sorry, wrong password")
+else:
+    print("Sorry, wrong username")
